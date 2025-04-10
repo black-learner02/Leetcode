8106707c0adc3c8c775e472cb64051c65d380c53
Ganesh M <ganesh45670@gmail.com>
2025-04-10 20:32:35 +0530

    Time: 0 ms (100%), Space: 50.1 MB (80.76%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -27,31 +27,16 @@ class Solution {
            {
              ans=nums[mid];
              break;
-           }
-           else if(nums[mid]==nums[mid+1])
-           {
-            if((mid+1)%2!=0)
-            {
-               
-                 low=mid+1;
-            }
-            else
-            {
-                 high=mid-1;
-            }
-           }
-           else
-           {
-            if((mid-1)%2!=0)
-            {
-                
-                high=mid-1;
-            }
-            else
-            {
-               low=mid+1;
-            }
-           }
+          }
+          else if(mid%2==0 && nums[mid]==nums[mid+1] || mid%2==1 && nums[mid]==nums[mid-1])
+          {
+              low=mid+1;
+          }
+          else
+          {
+            high=mid-1;
+          }
+
         }
         return ans;
     }
