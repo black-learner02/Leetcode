2bfc8f0713332c26c7067dd89deafbf9a3341e88
Ganesh M <ganesh45670@gmail.com>
2025-04-16 22:52:21 +0530

    Time: 19 ms (62.52%), Space: 58.1 MB (68.22%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -56,12 +56,17 @@ class Solution {
     public static int max(int[] arr)
     {
         int max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
 
         for(int i=0;i<arr.length;i++)
         {
             if(max<arr[i])
             {
                 max=arr[i];
+            }
+            if(arr[i]<min)
+            {
+                min=arr[i];
             }
         }
         return max;
