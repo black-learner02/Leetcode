4bd9a9195968294db735355d1089495c3ed4e058
Ganesh M <ganesh45670@gmail.com>
2025-04-22 19:44:59 +0530

    Time: 0 ms (100%), Space: 41.7 MB (98.58%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,40 +1,31 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        for(int i=0;i<matrix.length;i++)
+     int m=matrix[0].length;
+     int n=matrix.length;
+     int low=0;
+     int high=n*m-1;
+
+     while(low<=high)
+     {
+        int mid=(low+high)/2;
+
+        int row=mid/m;
+        int col=mid%m;
+
+        if(matrix[row][col]==target)
         {
-            if(matrix[i][0]<=target && target<=matrix[i][matrix[i].length-1])
-            {
-                return find(matrix[i],target);
-            }
+            return true;
         }
-
-        return false;
-        
-    }
-
-    public static boolean find(int[] arr, int tar)
-    {
-        int low=0;
-        int high=arr.length-1;
-
-        while(low<=high)
+        else if(matrix[row][col]<target)
         {
-            int mid=(low+high)/2;
-
-            if(arr[mid]==tar)
-            {
-                return true;
-            }
-            else if(arr[mid]>tar)
-            {
-                high=mid-1;
-            }
-            else
-            {
-                low=mid+1;
-            }
+            low=mid+1;
         }
-        return false;
+        else
+        {
+            high=mid-1;
+        }
+     }
+     return false;
     }
 }
