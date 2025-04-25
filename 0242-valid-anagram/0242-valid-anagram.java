b2dec5eb43ceb56528cf668e2e37e9141f0a5247
Ganesh M <ganesh45670@gmail.com>
2025-04-25 19:14:33 +0530

    Time: 18 ms (16.6%), Space: 45 MB (18.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -8,16 +8,12 @@ class Solution {
 
         HashMap<Character,Integer> map = new HashMap<>();
 
-        HashMap<Character,Integer> map1 = new HashMap<>();
-
         for(int i=0;i<s.length();i++)
         {
             map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-            map1.put(t.charAt(i),map1.getOrDefault(t.charAt(i),0)+1);
-
         }
 
-  /*      for(int i=0;i<t.length();i++)
+        for(int i=0;i<t.length();i++)
         {
            char ch = t.charAt(i);
            if(!map.containsKey(ch) || map.get(ch)==0)
@@ -26,10 +22,7 @@ class Solution {
            }
 
            map.put(ch,map.get(ch)-1);
-        }*/
-        if(map.equals(map1))
+        }
        return true;
-
-       return false;
     }
 }
