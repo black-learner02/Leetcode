3446eb4add43d71b1ae493263c19d611252933a0
Ganesh M <ganesh45670@gmail.com>
2025-04-25 19:02:38 +0530

    Time: 10 ms (8.59%), Space: 41.5 MB (50.89%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        String temp=s+s;
+
+        for(int i=0;i<s.length();i++)
+        {
+
+            String ran=temp.substring(i,i+s.length());
+            
+               if(ran.equals(goal))
+               {
+                return true;
+               }
+
+                 System.out.println(ran);
+            }
+
+        return false;
+        
+    }
+}
