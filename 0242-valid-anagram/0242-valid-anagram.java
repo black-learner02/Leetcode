67f76cc3250ea143f32758b53595f923da42b4f7
Ganesh M <ganesh45670@gmail.com>
2025-04-25 19:15:56 +0530

    Time: 6 ms (43.7%), Space: 43.2 MB (78.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,28 +1,44 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        if(s.length()!=t.length())
-        {
-            return false;
-        }
+    //     if(s.length()!=t.length())
+    //     {
+    //         return false;
+    //     }
 
-        HashMap<Character,Integer> map = new HashMap<>();
+    //     HashMap<Character,Integer> map = new HashMap<>();
 
-        for(int i=0;i<s.length();i++)
-        {
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-        }
+    //     for(int i=0;i<s.length();i++)
+    //     {
+    //         map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+    //     }
 
-        for(int i=0;i<t.length();i++)
-        {
-           char ch = t.charAt(i);
-           if(!map.containsKey(ch) || map.get(ch)==0)
-           {
-            return false;
-           }
+    //     for(int i=0;i<t.length();i++)
+    //     {
+    //        char ch = t.charAt(i);
+    //        if(!map.containsKey(ch) || map.get(ch)==0)
+    //        {
+    //         return false;
+    //        }
 
-           map.put(ch,map.get(ch)-1);
-        }
-       return true;
+    //        map.put(ch,map.get(ch)-1);
+    //     }
+    //    return true;
+
+    if (s.length() != t.length()) return false;
+
+    int[] count = new int[26];
+
+    for (int i = 0; i < s.length(); i++) {
+        count[s.charAt(i) - 'a']++;
+        count[t.charAt(i) - 'a']--;
     }
+
+    for (int c : count) {
+        if (c != 0) return false;
+    }
+
+    return true;
+ }
+
 }
