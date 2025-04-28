a17c2dd74d16148949b6f2a7204e77bbed21bd8c
Ganesh M <ganesh45670@gmail.com>
2025-04-28 18:56:33 +0530

    Time: 11 ms (15.3%), Space: 42.1 MB (17.62%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,26 +1,18 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-
-       HashMap<Character,Integer> map = new HashMap<>();
-       char res='0';
-
-       for(char c:s.toCharArray())
-       {
-        map.put(c,map.getOrDefault(c,0)+1);
-       }
-
-       for(char ch:t.toCharArray())
-       {
-           if(!map.containsKey(ch) || map.get(ch)==0)
-           {
-            res=ch;
-            break;
-           }
-           else
-           {
-            map.put(ch,map.get(ch)-1);
-           }
-       }
-       return res;
+        Map<Character, Integer> count = new HashMap<>();
+        
+        for (char c : t.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+        
+        for (char c : s.toCharArray()) {
+            count.put(c, count.get(c) - 1);
+            if (count.get(c) == 0) {
+                count.remove(c);
+            }
+        }
+        
+        return (char) count.keySet().toArray()[0];        
     }
 }
