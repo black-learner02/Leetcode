8b0474e87e22a69b1fc8e4406296ff3c2a598211
Ganesh M <ganesh45670@gmail.com>
2025-07-05 20:40:57 +0530

    Time: 13 ms (61.03%), Space: 53.2 MB (95.43%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
 
-      int res=0;
+     /* int res=0;
 
       int i=0;
       int j=0;
@@ -28,25 +28,66 @@ class Solution {
 
         if(count==k)
         {
-            if(nums[i]%2!=0)
+            int temp=i;
+            while(nums[temp]%2==0)
             {
-                res+=1;
+                temp++;
             }
-            else
-            {
-                int temp=i;
-                while(nums[temp]%2==0)
-                {
-                    temp++;
-                }
-
-                res+=temp-i+1;
-            }
+            res+=temp-i+1;
         }
 
         j++;
       }
 
-      return res;
+      return res;*/
+
+      int[] arr = new int[nums.length];
+
+      for(int i=0;i<nums.length;i++)
+      {
+         if(nums[i]%2==0)
+         {
+            arr[i]=0;
+         }
+         else
+         {
+            arr[i]=1;
+         }
+      }
+
+      return find(arr,k)-find(arr,k-1);
+    }
+     public int find(int[] nums,int goal)
+    {
+         if(goal<0)
+         {
+            return 0;
+         }
+
+        int res=0;
+
+        int i=0;
+        int j=0;
+        int sum=0;
+        while(j<nums.length)
+        {
+            sum+=nums[j];
+
+           
+            while(sum>goal)
+            {
+                sum-=nums[i];
+                i++;
+            }
+
+             if(sum<=goal)
+            {
+                res+=j-i+1;
+            }
+
+            j++;
+        }
+
+       return res;
     }
 }
