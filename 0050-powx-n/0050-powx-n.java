9abdef135ca9b8046f1b3f6a6bce0841bff22b1f
Ganesh M <ganesh45670@gmail.com>
2025-07-25 19:33:21 +0530

    Time: 0 ms (100%), Space: 42 MB (81.75%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,28 @@
 class Solution {
     public double myPow(double x, int n) {
 
-        
-        return Math.pow(x,n);
+       long pow = n;  // Convert to long to avoid overflow
+
+        if (pow < 0) {
+            x = 1 / x;
+            pow = -pow;
+        }
+
+        return fastPow(x, pow);
+    }
+
+    public double fastPow(double x, long n) {
+        double res = 1.0;
+
+        while (n > 0) {
+            if (n % 2 == 1) {  // If n is odd
+                res *= x;
+            }
+            x *= x;
+            n /= 2;
+        }
+
+       return res;
+
     }
 }
