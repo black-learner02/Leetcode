30b3e5b325d5bbe94e01dbff20613b5b7ad2e50c
Ganesh M <ganesh45670@gmail.com>
2025-09-17 18:42:23 +0530

    Time: 8 ms (3.45%), Space: 45 MB (8.04%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -8,75 +8,61 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ import java.math.BigInteger;
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-          StringBuilder first = new StringBuilder();
+        StringBuilder first = new StringBuilder();
+        StringBuilder sec = new StringBuilder();
 
-          StringBuilder sec = new StringBuilder();
+        ListNode a = l1;
+        ListNode b = l2;
 
-         long res=-1;
-
-         ListNode a=l1;
-         ListNode b=l2;
-
-         while(a!=null)
-         {
-             first.append(a.val);
-             a=a.next;
-         }
-
-        while(b!=null)
-        {
-            sec.append(b.val);
-            b=b.next;
+        while (a != null) {
+            first.append(a.val);
+            a = a.next;
         }
-      
-        
-         res=reverse(first)+reverse(sec);
 
-         System.out.println(res);
+        while (b != null) {
+            sec.append(b.val);
+            b = b.next;
+        }
 
-         if(res==0)
-         {
+        BigInteger num1 = reverse(first);
+        BigInteger num2 = reverse(sec);
+
+        BigInteger res = num1.add(num2);  // ✅ Corrected addition
+
+        if (res.equals(BigInteger.ZERO)) {  // ✅ Corrected comparison
             return new ListNode(0);
-         }
+        }
 
-         ListNode head=null;
+        // Convert res to string to extract digits
+        String resStr = res.toString();
+        ListNode head = null;
 
-         while(res!=0)
-         {
-            ListNode newNode = new ListNode((int)(res%10));
+        // Loop from end to start to get digits in reverse order
+        for (int i = resStr.length() - 1; i >= 0; i--) {
+            int digit = resStr.charAt(i) - '0';
+            ListNode newNode = new ListNode(digit);
 
-            if(head==null)
-            {
-                head=newNode;
-                res/=10;
-                continue;
+            if (head == null) {
+                head = newNode;
+            } else {
+                ListNode temp = head;
+                while (temp.next != null) {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
             }
-            
-            ListNode temp=head;
+        }
 
-            while(temp.next!=null)
-            {
-                temp=temp.next;
-            }
-
-            temp.next=newNode;
-            res/=10;
-            
-         }
-       
-
-     return head;
-
-
+        return head;
     }
 
-   public long reverse(StringBuilder num) {
-
-    String rev = num.reverse().toString();
-    return Long.parseLong(rev);
-}
-
+    public BigInteger reverse(StringBuilder num) {
+        String rev = num.reverse().toString(); 
+        return new BigInteger(rev);
+    }
 }
