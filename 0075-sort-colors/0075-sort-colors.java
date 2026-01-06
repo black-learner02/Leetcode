0a1deb850d889685b1d450719c4b10842c58c1e9
Ganesh M <ganesh45670@gmail.com>
2026-01-06 19:05:14 +0530

    Time: 0 ms (100%), Space: 42.2 MB (99.96%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,38 +1,70 @@
 class Solution {
     public void sortColors(int[] nums) {
-
-        int i=0;
-        int j=0;
-        int k=nums.length-1;
-
-        while(j<=k)
+    /*    int low,mid,high,temp;
+        low=0;
+        mid=0;
+        high=nums.length-1;
+        while(mid<=high)
         {
-            if(nums[j]==0 && i!=j)
-            {
-                swap(nums,i,j);
-                i++;
+            switch(nums[mid]){
+                case 0:{
+                temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                mid++;
+                low++;
+                break;
             }
-            else if(nums[j]==2)
+            case 1:
             {
-                 swap(nums,j,k);
-                 k--;
+                mid++;
+                break;
             }
+            case 2:
+            {
+                temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+                break;
+            }
+            }  
+        }  */
+
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+        int n=nums.length;
+
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+               swap(low,mid,nums);
+               low++;
+               mid++;
+            }
+
+            else if(nums[mid]==1)
+            {
+              mid++;
+            }
+
             else
             {
-                j++;
+              swap(mid,high,nums);
+              high--;
             }
+
         }
-        
-    }
 
-    public static int[] swap(int[] arr,int i,int j)
-    {
-        int temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+      }
 
-
-        return arr;
-    
-    }
+      void swap(int a,int b,int[] arr)
+      {
+        int temp;
+        temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+      }
 }
