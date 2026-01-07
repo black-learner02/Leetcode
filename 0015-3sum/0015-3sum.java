37a1ccf37089fd1ca1b2352acf8cfdaafb2a6e2e
Ganesh M <ganesh45670@gmail.com>
2026-01-07 19:07:14 +0530

    Time: 26 ms (99.13%), Space: 51.5 MB (100%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,43 +1,142 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+       
+   /*    HashSet<List<Integer>> set = new HashSet<>();
+       int n=nums.length;
 
-        Arrays.sort(nums);
-
-        Set<List<Integer>> set = new HashSet<>();
-        
-        int i=0;
-
-        while(i<nums.length-2)
+       for(int i=0;i<n;i++)
+       {
+        for(int j=i+1;j<n;j++)
         {
-            int j=i+1;
-            int k=nums.length-1;
-
-            while(j<k)
+            for(int k=j+1;k<n;k++)
             {
+                if(nums[i]+nums[j]+nums[k]==0)
+                {
+                List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]);
+                temp.sort(null);
+                set.add(temp);
+            }
+            }
+        }
+       }
 
+       List<List<Integer>> ans = new ArrayList<>(set);
+       return ans ;  */
+
+/*       int n=nums.length;
+       int temp;
+       //HashMap<Integer,Integer> map =new HashMap<>();
+       Set<List<Integer>> set = new HashSet<>();
+        for(int i=0;i<n;i++)
+       {
+        Set<Integer> se = new HashSet<>();
+        for(int j=i+1;j<n;j++)
+        {
+            temp=-(nums[i]+nums[j]);
+            if(se.contains(temp))
+            {
+               List<Integer> tem=Arrays.asList(nums[i],nums[j],temp);
+               tem.sort(null);
+               set.add(tem);
+            }
+            se.add(nums[j]);
+        }
+       }
+       List<List<Integer>> list = new ArrayList<>(set);
+       return list;  */
+
+       int n=nums.length;
+       Arrays.sort(nums);
+
+       List<List<Integer>> list = new ArrayList<>();
+
+       for(int i=0;i<n;i++)
+       {
+        if(i!=0 && nums[i]==nums[i-1])
+        {
+            continue;
+        }
+
+        int j=i+1;
+        int k=n-1;
+        while(j<k)
+        {
             int sum=nums[i]+nums[j]+nums[k];
 
-            if(sum==0)
-            {
-                 set.add(Arrays.asList(nums[i],nums[j],nums[k]));
-                 j++;
-            }
-            else if(sum<0)
+            if(sum<0)
             {
                 j++;
             }
-            else
+            else if(sum>0)
             {
                 k--;
             }
-
-            }
-            i++;
+            else
+            {
+                List<Integer> temp=Arrays.asList(nums[i],nums[j],nums[k]);
+                list.add(temp);
+                
+            
+        
+        
+        while(j<k && nums[j]==nums[j+1])
+        {
+            j++;
+        }
+        while(j<k && nums[k]==nums[k-1])
+        {
+            k--;
         }
 
-        List<List<Integer>> list = new ArrayList<>(set);
-
-        return list;
+        j++;
+        k--;
         
-    }
+            }
+       }
+       
+       }
+     return list; 
+
+   /*   int n = nums.length;
+        Arrays.sort(nums);  // Sorting for two-pointer approach
+        
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < n - 2; i++) { // Ensure at least 3 elements left
+            if (i > 0 && nums[i] == nums[i - 1]) { // Skip duplicate elements for i
+                continue;
+            }
+
+            int j = i + 1;
+            int k = n - 1;
+            
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    // Found a valid triplet
+                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    
+                    // Move both pointers to next unique values
+                    j++;
+                    k--;
+
+                    // **Skip all duplicate elements for j**
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+
+                    // **Skip all duplicate elements for k**
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                }
+            }
+        }
+        return list;  */
+          }
 }
