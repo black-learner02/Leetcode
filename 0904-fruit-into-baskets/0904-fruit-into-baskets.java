dec1f8e6c139701d91b2793b02380026ece84829
Ganesh M <ganesh45670@gmail.com>
2026-01-22 13:09:31 +0530

    Time: 35 ms (89.65%), Space: 69.7 MB (75.03%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,66 +1,81 @@
 class Solution {
     public int totalFruit(int[] fruits) {
 
-    // int res=0;
-    // int i=0;
-    // int j=0;
 
-    // HashMap<Integer,Integer> map = new HashMap<>();
+        int res=0;
+    int i=0;
+    int j=0;
 
-    // while(j<fruits.length)
-    // {
+    HashMap<Integer,Integer> map = new HashMap<>();
 
-    //    if(map.containsKey(fruits[j]))
-    //    {
-    //       map.put(fruits[j],j);
-    //    }
-    //    else
-    //    {
-    //      if(map.size()==2)
-    //      {
-    //         while(fruits[i]==fruits[j-1])
-    //         {
-    //             i++;
-    //         }
-    //         i=map.get(fruits[i])+1;
-    //         int temp=map.get(fruits[i]);
-    //         map.clear();
-    //         map.put(fruits[j],j);
-    //         map.put(fruits[i],temp);
-    //      }
-    //      else
-    //      {
-    //         map.put(fruits[j],j);
-    //      }
-    //    }
+    while(j<fruits.length)
+    {
 
-    //     res=Math.max(res,j-i+1);
-
-
-    //    j++;
-    
-
-    // } 
-
-    //     return res;
-
-    int i = 0, max = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for (int j = 0; j < fruits.length; j++) {
-            map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
-
-            while (map.size() > 2) {
-                map.put(fruits[i], map.get(fruits[i]) - 1);
-                if (map.get(fruits[i]) == 0) {
-                    map.remove(fruits[i]);
-                }
+       if(map.containsKey(fruits[j]))
+       {
+          map.put(fruits[j],j);
+       }
+       else
+       {
+         if(map.size()==2)
+         {
+            while(fruits[i]==fruits[j-1])
+            {
                 i++;
             }
+            i=map.get(fruits[i])+1;
+            int temp=map.get(fruits[i]);
+            map.clear();
+            map.put(fruits[j],j);
+            map.put(fruits[i],temp);
+         }
+         else
+         {
+            map.put(fruits[j],j);
+         }
+       }
 
-            max = Math.max(max, j - i + 1);
-        }
+        res=Math.max(res,j-i+1);
 
-        return max;
+
+       j++;
+    
+
+    } 
+
+        return res;
+
+
+   /*    Map<Integer,Integer> map = new HashMap<>();
+
+       int i=0;
+       int j=0;
+
+       int len=Integer.MIN_VALUE;
+
+       while(j<fruits.length)
+       {
+          map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+
+          if(map.size()<=2)
+          {
+            len=Math.max(len,j-i+1);
+          }
+
+          while(map.size()>2)
+          {
+             map.put(fruits[i],map.get(fruits[i])-1);
+
+             if(map.get(fruits[i])==0)
+             {
+                map.remove(fruits[i]);
+             }
+             i++;
+          }
+         
+         j++;
+       } 
+       return len;*/
+
     }
 }
