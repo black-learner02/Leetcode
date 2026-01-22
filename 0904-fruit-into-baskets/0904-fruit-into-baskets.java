21c40307f1e1f7e6a6f97db92273ef5b68789761
Ganesh M <ganesh45670@gmail.com>
2026-01-22 13:08:07 +0530

    Time: 54 ms (64.12%), Space: 70.8 MB (24.9%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,36 +1,66 @@
 class Solution {
     public int totalFruit(int[] fruits) {
 
-       Map<Integer,Integer> map = new HashMap<>();
+    // int res=0;
+    // int i=0;
+    // int j=0;
 
-       int i=0;
-       int j=0;
+    // HashMap<Integer,Integer> map = new HashMap<>();
 
-       int len=Integer.MIN_VALUE;
+    // while(j<fruits.length)
+    // {
 
-       while(j<fruits.length)
-       {
-          map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+    //    if(map.containsKey(fruits[j]))
+    //    {
+    //       map.put(fruits[j],j);
+    //    }
+    //    else
+    //    {
+    //      if(map.size()==2)
+    //      {
+    //         while(fruits[i]==fruits[j-1])
+    //         {
+    //             i++;
+    //         }
+    //         i=map.get(fruits[i])+1;
+    //         int temp=map.get(fruits[i]);
+    //         map.clear();
+    //         map.put(fruits[j],j);
+    //         map.put(fruits[i],temp);
+    //      }
+    //      else
+    //      {
+    //         map.put(fruits[j],j);
+    //      }
+    //    }
 
-          if(map.size()<=2)
-          {
-            len=Math.max(len,j-i+1);
-          }
+    //     res=Math.max(res,j-i+1);
 
-          while(map.size()>2)
-          {
-             map.put(fruits[i],map.get(fruits[i])-1);
 
-             if(map.get(fruits[i])==0)
-             {
-                map.remove(fruits[i]);
-             }
-             i++;
-          }
-         
-         j++;
-       } 
-       return len;
+    //    j++;
+    
 
+    // } 
+
+    //     return res;
+
+    int i = 0, max = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int j = 0; j < fruits.length; j++) {
+            map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+
+            while (map.size() > 2) {
+                map.put(fruits[i], map.get(fruits[i]) - 1);
+                if (map.get(fruits[i]) == 0) {
+                    map.remove(fruits[i]);
+                }
+                i++;
+            }
+
+            max = Math.max(max, j - i + 1);
+        }
+
+        return max;
     }
 }
